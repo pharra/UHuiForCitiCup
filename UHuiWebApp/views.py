518f9b0a8375fb2331263aedfd22bc2bff8daf98
丰 王 <940530348@qq.com>
2017-07-12 11:17:30 +0800

    init

--- UHuiWebApp/views.py
+++ UHuiWebApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'U.html', {
+            'question': "a",
+            'error_message': "You didn't select a choice.",
+        })
