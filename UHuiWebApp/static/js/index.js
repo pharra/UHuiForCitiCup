8e45a562129982d9b3b43b8a69b316cae168679b
丰 王 <940530348@qq.com>
2017-07-13 13:28:53 +0800

    fix the bug because of the merge

--- UHuiWebApp/static/js/index.js
+++ UHuiWebApp/static/js/index.js
@@ -2,7 +2,6 @@ $(".category-list-a").hover(
     function(event) {
         $($(this).attr("href")).css("height", $('#myCarousel').height());
         $($(this).attr("href")).css("width", $('#myCarousel').width());
-
         $($(this).attr("href")).removeClass("being-hidden");
         $($(this).attr("href")).css("left", $('#myCarousel').offset().left);
         $($(this).attr("href")).css("top", $('#myCarousel').offset().top);
