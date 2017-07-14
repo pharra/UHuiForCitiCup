8cc9e273fb446622f354114a242c169855c0d4b3
Sirius5272 <527293764@qq.com>
2017-07-14 15:07:38 +0800

    Signed-off-by: Sirius5272 <527293764@qq.com>

--- UHuiWebApp/views.py
+++ UHuiWebApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from UHuiWebApp.models import  user
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -11,3 +11,14 @@ def index(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def signUp(request):
+    emailAddress = request.GET['email',0]
+    phoneNumber = request.GET['phoneNum',0]
+    password = request.GET['password']
+    if(emailAddress != 0):
+        # 将邮箱作为用户名存入数据库中
+        newUser = user()
+    elif(phoneNumber!=0):
+        # 将手机号作为用户名存入数据库中
+
