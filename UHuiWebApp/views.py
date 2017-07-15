9e79a8a5bd531dd0b9189e2a2da1aeab5d332d67
Johnson <z929527511@outlook.com>
2017-07-15 15:26:16 +0800

    contribute to sign_up, finish login, test needed

--- UHuiWebApp/views.py
+++ UHuiWebApp/views.py
@@ -3,8 +3,8 @@ from UHuiWebApp import models
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import hashlib
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
+import time
+import random
 import json
 
 
@@ -12,11 +12,23 @@ import json
 
 
 # 普通函数
-def encode(md5):
+def encryption(md5):
     key = 'UHuiForCiti'
     m = hashlib.md5()
     m.update(md5.join(key).encode("UTF-8"))
     return m.hexdigest()
+
+
+def randomID():
+    signUpTime = int(time.time())
+    for i in range(0, 4):
+        append = append + random.random('abcdefghijklmopqrstuvwxyz')
+    ID = "%d%s" % (signUpTime, append)
+    if models.User.objects.filter(ID=ID).count() != 0:
+        return randomID()
+    return ID
+
+
 
 
 # get方法函数
@@ -35,41 +47,55 @@ def post_login(request):
     #     u_name = cookie_content.split("_")[0]
 
     u_name = request.POST.get('username')
+    # 通过@判断用户名为email/手机号
     if "@" in u_name:
+        # 查询用户是否存在
         user = models.User.objects.filter(email=u_name)
         if user is None:
-            return None
+            return HttpResponse(u"用户不存在", content_type="text/plain")
         pswObj = models.User.objects.get(email=u_name)
-
     else:
         user = models.User.objects.filter(phonenum=u_name)
         if user is None:
-            return None
+            return HttpResponse(u"用户不存在", content_type="text/plain")
         pswObj = models.User.objects.get(phonenum=u_name)
 
-    psw = encode(request.POST.get('password'))
+    psw = encryption(request.POST.get('password'))
     password = bytes.decode(pswObj.password.encode("UTF-8"))
     if psw == password:
+        # 返回cookie，在浏览器关闭前维持登录状态
         response = HttpResponseRedirect("/")
-        value = u_name + "_" + encode(u_name + psw)
+        value = u_name + "_" + encryption(u_name + psw)
         response.set_cookie(key="uhui", value=value, httponly=True)
         return response
     else:
-        return None
+        return HttpResponse(u"密码错误", content_type="text/plain")
 
 
 def post_signUp(request):
-    emailAddress = request.POST.get('email', 0)
-    _nickname = request.POST.get('nickname', 0)
-    phoneNumber = request.POST.get('phoneNum', 0)
-    password = request.POST.get('password')
+    username = request.POST.get('username')
+    nickname = request.POST.get('nickname')
+    password = encryption(request.POST.get('password'))
+    gender = request.POST.get('gender')
+
 
     # 查询数据库中昵称是否存在
-    if models.user.objects.filter(nickname=_nickname):
-        return
-    if emailAddress != 0:
+    if models.User.objects.filter(nickname=nickname):
+        return HttpResponse(u"昵称已存在", content_type="text/plain")
+    if '@' in username:
+        if models.User.objects.filter(email=username).count() != 0:
+            return HttpResponse(u"邮箱已被注册", content_type="text/plain")
+
+        # 邮箱验证
         # 将邮箱作为用户名存入数据库中
-        newUser = models.user(email=emailAddress)
-    elif phoneNumber != 0:
-        # 将手机号作为用户名存入数据库中
+        models.User.objects.create(ID=randomID(), nickname=nickname, password=password, gender=gender, email=username)
+        return HttpResponse(u"请检查验证邮件", content_type="text/plain")
+    else:
+        if models.User.objects.filter(phonenum=username).count() != 0:
+            return HttpResponse(u"手机号已被注册", content_type="text/plain")
+        # 短信验证码验证
         pass
+        # 将手机号作为用户名存入数据库中
+        models.User.objects.create(ID=randomID(), nickname=nickname, password=password, gender=gender,
+                                   phoneNum=username)
+        return HttpResponse(u"注册成功", content_type="text/plain")
