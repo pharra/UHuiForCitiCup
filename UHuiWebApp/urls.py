e602fa23116c8afeb0a1f1fa40938478249f8f21
Max <1025933456@qq.com>
2017-07-17 15:58:34 +0800

    一点微小的贡献

--- UHuiWebApp/urls.py
+++ UHuiWebApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
+    url(r'^user$', views.user, name='user'),
     url(r'^post_login$', views.post_login, name='post_login'),
     url(r'^post_signup$', views.post_signUp, name='post_signUp'),
     url(r'^post_userInfo$', views.post_userInfo, name='post_userInfo'),
