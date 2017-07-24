05b0648ac78e6d78133b9066d79bf0c4137fd817
pharra <940530348@qq.cm>
2017-07-24 09:58:40 +0800

    add search

---
+++ UHuiWebApp/static/js/search.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wf on 17-7-24.
+ */
+function search() {
+    $.ajax({
+        url: '/post_search',
+        type: 'POST',
+        dataType: 'json',
+        data: { "keyWord": $("#nav-search").val() },
+        timeout: 3000,
+        cache: false,
+    });
+}
