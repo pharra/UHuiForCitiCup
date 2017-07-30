65ee557c0805c8e65bb2636c277be8ace90c3722
pharra <940530348@qq.com>
2017-07-30 20:06:33 +0800

    add coupon info

--- UHuiWebApp/static/js/coupons.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wf on 17-7-30.
- */
-
-$("div[id^='couponsid_']").click(function () {
-    var couponsid = $(this).attr(id).replace("couponsid_",'');
-    window.location.href = "/commodity?couponsID="+couponsid;
-});
