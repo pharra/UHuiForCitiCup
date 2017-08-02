525d5caf361ff86511d0631fc8eba3cdd81c2d46
Johnson <z929527511@outlook.com>
2017-08-02 21:47:56 +0800

    a lot of bugs

--- UHuiProject/__init__.py
+++ UHuiProject/__init__.py
@@ -1,9 +1,14 @@
+from wsgiref import headers
+
 import pymysql
 from django.http import HttpResponse
 
 from UHuiProject.settings import DEBUG
 
+
+
 pymysql.install_as_MySQLdb()
 HttpResponse.type = 'HttpResponse'
 if DEBUG is True:
     print('http')
+
