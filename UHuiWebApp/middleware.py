eb08d8b3a7b414945fc80552dbd7f8ea025d564a
Johnson <z929527511@outlook.com>
2017-08-03 21:55:23 +0800

    add message

--- UHuiWebApp/middleware.py
+++ UHuiWebApp/middleware.py
@@ -50,14 +50,18 @@ class SimpleMiddleware(object):
             if isinstance(response, dict):
                 response = JsonResponse(response)
             elif request.uid is not None and response.type == "JsonResponse":
-                print(response.content)
+                # print(response.content)
                 content = json.loads(bytes.decode(response.content))
                 userinfo = views.post_userInfo(request.uid)
+                message = views.post_getMessage(request)
                 for key in userinfo:
                     content[key] = userinfo[key]
+                for key in message:
+                    content[key] = message[key]
                 response.content = json.dumps(content)
             elif request.uid is not None and response.type == "render":
                 response.addContent(views.post_userInfo(request.uid))
+                response.addContent(views.post_getMessage(request))
 
         # Code to be executed for each request/response after
         # the view is called.
