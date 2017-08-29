a97e00cdad582831b0214005d54e4413b83e7053
pharra <940530348@qq.com>
2017-08-29 12:44:34 +0800

    add lazy-flash images

--- UHuiWebApp/static/js/preimg.js
+++ UHuiWebApp/static/js/preimg.js
@@ -1,8 +1,15 @@
 Vue.component('coupon-img', {
-    template: '<img :src="url" >',
+    template: '<div class="spinner" v-if="loading">\n' +
+        '    <div class="rect1"></div>\n' +
+        '    <div class="rect2"></div>\n' +
+        '    <div class="rect3"></div>\n' +
+        '    <div class="rect4"></div>\n' +
+        '    <div class="rect5"></div>\n' +
+        '  </div> ' +
+        '<img :src="url" v-else>',
     props: ['imgurl'],
     data: function () {
-        return {url: '/static/images/loader.gif'}
+        return {url: '',loading:true}
     },
     created: function () {
         var _self = this;
@@ -10,7 +17,52 @@ Vue.component('coupon-img', {
         var img = new Image();
         img.src = url;
         img.onload = function () {
+            _self.loading = false;
             _self.url = url;
         }
     }
 });
+
+
+
+
+
+
+
+
+/*
+Vue.component('pre-img', {
+        template: '<div class="spinner" v-if="loading">\n' +
+        '    <div class="rect1"></div>\n' +
+        '    <div class="rect2"></div>\n' +
+        '    <div class="rect3"></div>\n' +
+        '    <div class="rect4"></div>\n' +
+        '    <div class="rect5"></div>\n' +
+        '  </div> ' +
+        '<img :src="imgurl" v-else-if="loaded">',
+        props:['imgurl'],
+        computed:{
+            loading:function () {
+                return !this.imgurl;
+            },
+            loaded:function () {
+                return !this.loading;
+            }
+        }
+    });
+    let vueimg = new Vue({
+        el:'.vue-img',
+        data:{
+            url:''
+        },
+        methods:{
+            getimgurl:function (url) {
+                let _self = this;
+                let img = new Image();
+                img.src = url;
+                img.onload = function () {
+                    _self.url = url;
+                }
+            }
+        }
+    });*/
